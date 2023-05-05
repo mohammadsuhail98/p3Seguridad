b2727f018fba3a96a10d5c6f24b4fc155b53a6be
elena-dan-at-892988686872 <e.rathsponce@gmail.com>
2023-05-05 21:05:54 +0200

    deleted unnecessary code (@app.route)

--- AnonimizationService.py
+++ AnonimizationService.py
@@ -1,22 +1,7 @@
 from cryptography.hazmat.primitives import hashes, hmac
 from cryptography.fernet import Fernet
-from faker import Faker
-from flask import Flask, request, jsonify
 import pandas as pd
-import hashlib
 import numpy as np
-
-fake = Faker()
-
-# Generate dummy data
-data = {
-    "Patient Name": [fake.name() for _ in range(100)],
-    "Patient ID": [fake.uuid4() for _ in range(100)],
-    "Medical Condition": [fake.word() for _ in range(100)],
-    "Medications": [fake.word() for _ in range(100)],
-    "Date of Birth": [fake.date_of_birth().strftime("%Y-%m-%d") for _ in range(100)],
-    "Social Security Number": [fake.ssn() for _ in range(100)],
-}
 
 
 def anonimyze_with_hmac(df: pd.DataFrame, columns: list, key: str) -> tuple:
@@ -53,8 +38,9 @@ def perturbe_numeric_columns(df: pd.DataFrame, columns: list) -> pd.DataFrame:
         aux_df[column] = aux_df[column] + noise
         aux_df[column] = aux_df[column].round(0)
         aux_df[column] = aux_df[column].apply(lambda x: 0 if x < 0 else x)
-
     return aux_df
+
+
 def micro_aggregation(df: pd.DataFrame, columns: list) -> pd.DataFrame:
     aux_df = df.copy()
     group_size = 10
@@ -112,6 +98,7 @@ def reach_k_anonymity(
         steps += 1
     return aux_df
 
+
 def drop_columns_and_save(input_file, output_file, keep_columns):
     df = pd.read_csv(input_file, encoding='latin-1')
     drop_columns = [col for col in df.columns if col not in keep_columns]
@@ -120,6 +107,7 @@ def drop_columns_and_save(input_file, output_file, keep_columns):
     df = df.loc[~df.applymap(lambda x: '-' in str(x)).any(axis=1)]
     df = df.dropna()
     df.to_csv(output_file, index=False)
+
 
 def generalize_categorical_columns(df: pd.DataFrame, columns_hierarchies: dict):
     aux_df = df.copy()
@@ -131,116 +119,7 @@ def generalize_categorical_columns(df: pd.DataFrame, columns_hierarchies: dict):
             ] = hierarchy
 
     return aux_df
-# Create dataframe from data
-df = pd.DataFrame(data)
 
-# Save dataframe to CSV file
-df.to_csv("dummy_dataset.csv", index=False)
-
-app = Flask(__name__)
-datasets = {}
-
-
-# Import dataset
-@app.route("/import", methods=["POST"])
-def import_data():
-    file_path = request.form["file_path"]
-    data = pd.read_csv(file_path)
-    datasets[file_path] = data
-    return jsonify(data.to_dict()), 200
-
-
-# Anonymize dataset
-@app.route("/anonymize", methods=["POST"])
-def anonymize_data():
-    file_path = request.form["file_path"]
-    data = datasets[file_path]
-    # Add your anonymization code here
-    anonymized_data = data.copy()
-    mapping_data = pd.DataFrame(
-        columns=["column_name", "pseudonym", "original_identifier"]
-    )
-    for col in anonymized_data.columns:
-        # Generate a random pseudonym for each column
-        pseudonym = hashlib.md5(fake.word().encode("utf-8")).hexdigest()[:8]
-        # Replace the column values with pseudonyms
-        mapping_data = mapping_data.append(
-            {"column_name": col, "pseudonym": pseudonym}, ignore_index=True
-        )
-        anonymized_data[col] = pseudonym
-    # Save the mapping between the original identifiers and the pseudonyms
-    mapping_data.to_csv(f"{file_path}_mapping.csv", index=False)
-    datasets[file_path] = anonymized_data
-    return jsonify(anonymized_data.to_dict()), 200
-
-
-# Substitute pseudonyms with original identifiers
-@app.route("/substitute", methods=["POST"])
-def deidentify_data():
-    file_path = request.form["file_path"]
-    data = datasets[file_path]
-    # Add your deidentification code here
-    deidentified_data = data.copy()
-    for col in deidentified_data.columns:
-        # Retrieve the mapping between the pseudonym and the original identifier
-        if "ID" in col or "Social Security Number" in col:
-            mapping = datasets[f"{file_path}_{col}"].rename(columns={col: "pseudonym"})
-            # Replace the pseudonyms with the original identifier values
-            deidentified_data[col] = pd.merge(
-                deidentified_data[[col]], mapping, on="pseudonym", how="left"
-            )[col].fillna("Unknown")
-    datasets[file_path] = deidentified_data
-    return jsonify(deidentified_data.to_dict()), 200
-
-
-# Suppress dataset
-@app.route("/suppress", methods=["POST"])
-def suppress_data():
-    file_path = request.form["file_path"]
-    data = datasets[file_path]
-    # Add your suppression code here
-    suppressed_data = data.copy()
-    # Example suppression: remove the 'Social Security Number' and 'Patient ID' columns
-    suppressed_data = suppressed_data.drop(
-        ["Social Security Number", "Patient ID"], axis=1
-    )
-    datasets[file_path] = suppressed_data
-    return jsonify(suppressed_data.to_dict()), 200
-
-
-# Generalize numeric data
-@app.route("/generalize", methods=["POST"])
-def generalize_data():
-    file_path = request.form["file_path"]
-    data = datasets[file_path]
-    # Add your generalization code here
-    generalized_data = data.copy()
-    for col in generalized_data.columns:
-        # Check if the column contains numeric data
-        if generalized_data[col].dtype in ["int64", "float64"]:
-            # Find the minimum and maximum values in the column
-            min_val = generalized_data[col].min()
-            max_val = generalized_data[col].max()
-            # Generalize the values in the column by mapping them to ranges
-            range_size = (max_val - min_val) / 5
-            generalized_data[col] = pd.cut(
-                generalized_data[col],
-                bins=5,
-                labels=[
-                    f"{min_val:.2f}-{min_val+range_size:.2f}",
-                    f"{min_val+range_size:.2f}-{min_val+(range_size*2):.2f}",
-                    f"{min_val+(range_size*2):.2f}-{min_val+(range_size*3):.2f}",
-                    f"{min_val+(range_size*3):.2f}-{min_val+(range_size*4):.2f}",
-                    f"{min_val+(range_size*4):.2f}-{max_val:.2f}",
-                ],
-            )
-    datasets[file_path] = generalized_data
-    return jsonify(generalized_data.to_dict()), 200
-
-"""
-if __name__ == "__main__":
-    app.run()
-"""
 
 if __name__ == "__main__":
     key = None
